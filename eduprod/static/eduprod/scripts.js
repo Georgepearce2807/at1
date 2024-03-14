033cfa009edbb4290e238120343ced4ce37b5f0f
Georgepearce2807 <georgep2807@gmail.com>
2024-03-15 09:46:50 +1100

    Eduprod v1.2 - Page chooses 3 random questions from question database before resetting quiz (added desiredQuestionAmount variable)

--- eduprod/static/eduprod/scripts.js
+++ eduprod/static/eduprod/scripts.js
@@ -1,17 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     let currentQuestionIndex = 0;
+    let questionsDisplayed = 0;                                            // <---- Adding variable to measure number of displayed questions
+    let desiredQuestionAmount = 3;                                       
     const questions = JSON.parse(document.getElementById('content').getAttribute('data-questions'));
     const content = document.getElementById('content');
     const btn = document.getElementById('revealBtn');
 
     function displayQuestion() {
-        if (currentQuestionIndex < questions.length) {
+        if (currentQuestionIndex < questions.length && questionsDisplayed < desiredQuestionAmount) {
             const question = questions[currentQuestionIndex].fields.question_text;
             const answer = questions[currentQuestionIndex].fields.answer_text;
             content.innerHTML = `<div class='question'>Question: ${question}</div><div class='answer' style='display: none;'>Answer: ${answer}</div>`;
             btn.textContent = "Reveal Answer";
-        } else {
-            content.innerHTML = "Reset quiz";
+        } 
+        else {
+            content.innerHTML = "Reset quiz";                             // <---- Changes button to say Reset
             btn.textContent = "Reset";
         }
     }
@@ -24,16 +27,21 @@ document.addEventListener("DOMContentLoaded", function() {
          }
      }
 
-    shuffleQuestions();
+    shuffleQuestions();                                                  // <---- Runs the functions made above 
     displayQuestion();
 
-    btn.addEventListener("click", function() {
+    btn.addEventListener("click", function() {                           // <---- Reveals answer and sets button text to Next Question
         const answerElement = content.querySelector('.answer');
         if (btn.textContent === "Reveal Answer") {
             answerElement.style.display = "block";
             btn.textContent = "Next Question";
-        } else {
-            currentQuestionIndex++;
+            questionsDisplayed++;
+        } 
+        else if (btn.textContent === "Reset") {                          // <---- Reset button which reloads the page upon Reset
+            window.location.reload();
+        }
+        else {
+            currentQuestionIndex++;                                      // <---- Adds one to the Question index to proceed throught the questions list
             displayQuestion();
         }
     });
