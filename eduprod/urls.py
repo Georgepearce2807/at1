687471894923762321a97c61d12620ad91eaded4
Georgepearce2807 <georgep2807@gmail.com>
2024-03-18 15:00:06 +1100

    Eduprod v1.3 - Added multiple pages and very basic page content

--- eduprod/urls.py
+++ eduprod/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('ratel', views.ratel, name='ratel'),
+    path('quiz', views.quiz, name='quiz'),
 ]
--- eduprod/views.py
+++ eduprod/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render
 from .models import Question
 
 def index(request):
+    return render(request, 'eduprod/index.html')
+
+def ratel(request):
+    return render(request, 'eduprod/ratel.html') 
+
+def quiz(request):
     questions = Question.objects.all()
     questions_json = serializers.serialize('json', questions)
-    return render(request, 'eduprod/index.html', {'questions_json': questions_json})
+    return render(request, 'eduprod/quiz.html', {'questions_json': questions_json}) 
